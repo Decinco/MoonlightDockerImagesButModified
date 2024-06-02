f45f6bd0bbfaa4b95f6128c15f02164fd2772f24
Jesper <94371018+tnsjesper@users.noreply.github.com>
2024-06-02 15:27:33 +0200

    Build

--- apps/ts3/entrypoint.sh
+++ apps/ts3/entrypoint.sh
@@ -9,5 +9,5 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
+# Run the Server 
 eval ${MODIFIED_STARTUP}
