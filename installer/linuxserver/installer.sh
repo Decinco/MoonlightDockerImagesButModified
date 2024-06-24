875d75943c36f92ee3dba604c38ca51619479729
Jesper <94371018+tnsjesper@users.noreply.github.com>
2024-06-24 20:00:54 +0200

    Update installer.sh

--- installer/linuxserver/installer.sh
+++ installer/linuxserver/installer.sh
@@ -23,6 +23,17 @@ echo "${nc}"
 if [[ -f "./installed" ]]; then
     echo "${bold}${lightgreen}Container is now ready${lightgreen}"
     echo "    Starting WebConsole - Port: {SERVER_PORT}"
+    function runcmd1 {
+        read -r cmdtorun
+        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd
+    }
+    function runcmd {
+        read -r cmdtorun
+        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd1
+    }
+    runcmd
     /home/container/usr/bin/gotty --port ${SERVERPORT} --credential root:${LOGINPASSWORD} --permit-write --permit-arguments --title-format 'WebConsole - Moonlight' bash
 else
     echo "Downloading files for application"
@@ -68,5 +79,16 @@ echo "${nc}"
     
     echo "${bold}${lightgreen}Container is now ready${lightgreen}"
     echo "    Starting WebConsole - Port: {SERVER_PORT}"
+    function runcmd1 {
+        read -r cmdtorun
+        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd
+    }
+    function runcmd {
+        read -r cmdtorun
+        ./libraries/proot -S . /bin/bash -c "$cmdtorun"
+        runcmd1
+    }
+    runcmd
     /home/container/usr/bin/gotty --port ${SERVERPORT} --credential root:${LOGINPASSWORD} --permit-write --permit-arguments --title-format 'WebConsole - Moonlight' bash
 fi
